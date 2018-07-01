216f2a2f5404d18b5ac217ad405f838843ca5ee5
GabiGII <40599724+GabiGII@users.noreply.github.com>
2018-07-01 23:17:47 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,31 @@
 // Select color input
 // Select size input
-
+$('#pixelCanvas').submit(function event() {
+   event.preventDefault();
+   height = $('#inputHeight').val();
+   width = $('#inputWidth').val();
+   makeGrid(height, width);
+});
+$('#submitButton').on('click', makeGrid);
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
-// Your code goes here!
+ // Your code goes here!
+ $('tr').remove();
+ for (let u = 0; u < row; u++) {
+     $().appendTo('<tr id=table' + i + '></tr>');
+     for (let v = 0; v < column; v++) {
+         $('#table' + i).append('<td></td>');
+       }
+   }
+   // add colors to the cells
+   $('td').click(function addColor(){
+       color = $("#colorPicker").val();
+       if ($(this).attr('style')) {
+           $(this).removeAttr('style')
+       } else {
+           $(this).attr('style', 'background-color:' + color);
+       }
+   })
 
 }
